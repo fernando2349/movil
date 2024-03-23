c44b2dd625aae993e15faff8513b8b4dd7971cd4
fernando2349 <146887271+fernando2349@users.noreply.github.com>
2024-03-23 18:48:48 -0500

    main
    plantillas

---
+++ myappmovil/src/component/RoundedButton.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { MyColors } from '../theme/AppTheme'
+import { TouchableOpacity, Text, StyleSheet } from 'react-native'
+//elemento
+export const RoundedButton = ({text, onPress}: Props) => {
+ return ( 
+    <TouchableOpacity 
+        style={styles.RoundedButton} 
+        onPress={() => onPress()} 
+    > 
+        <Text style={styles.textButton} >{ text}</Text> 
+    </TouchableOpacity>  
+) 
+}
+
+const styles = StyleSheet.create({
+ RoundedButton: {
+ width: '100%',
+ height: 50,
+ backgroundColor: MyColors.primary, 
+ alignItems: 'center',
+ justifyContent: 'center',
+ borderRadius: 15,
+ },
+ textButton: {
+ color: 'white',
+ }
+});
+
+interface Props { 
+    text: string; 
+    onPress: () => void, 
+ 
+} 
